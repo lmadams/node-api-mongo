3b3a397611fb9385b82c807701870b14cfce7abf
Lucas Adams <adams.lucass@gmail.com>
2017-09-05 22:08:34 -0300

    alterado cors

--- src/app.js
+++ src/app.js
@@ -7,7 +7,9 @@ const app = express();
 // const router = express.Router();
 
 // Connecta ao banco
-mongoose.connect(config.connectionString);
+mongoose.connect(config.connectionString, {
+  useMongoClient: true,
+});
 
 // Carregar os Models
 require('./models/product');
@@ -26,10 +28,22 @@ app.use(bodyParser.json({
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Habilita o CORS
-app.use((req, res) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+app.use((req, res, next) => {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  // res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
 });
 
 app.use('/', indexRoute);
